11f0f2267766a28d90029a4b0b496fe5be9a1bfc
Arsene Kevin <kevinkouomeu@icloud.com>
2020-12-27 12:20:21 +0100

    scenario passed & disabling messages

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -4,24 +4,33 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.junit.Assert.assertEquals;
+
+class Friday {
+    static String checkIfItIs(String today) {
+        return today.equals("Sunday") ? "Nope" : "Yes";
+    }
+}
+
+
 public class StepDefinitions {
+
+    private String today;
+    private String actualAnswer;
 
     @Given("today is Sunday")
     public void today_is_sunday() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        today = "Sunday";
     }
 
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_friday_yet() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("I should be told {string}")
-    public void i_should_be_told(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        actualAnswer = Friday.checkIfItIs(today);
     }
 
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expected) {
+        assertEquals(expected, actualAnswer);
+    }
 
 }
